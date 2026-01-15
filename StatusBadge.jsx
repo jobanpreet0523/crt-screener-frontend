e180d18bb642e1262f141d0e14f82d4e10914727
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-15 23:52:30 +0530

    Create StatusBadge.jsx

---
+++ StatusBadge.jsx
@@ -0,0 +1,12 @@
+export default function StatusBadge({ status }) {
+  const colors = {
+    CRT_CONFIRMED: "green",
+    NEAR_CRT: "orange"
+  };
+
+  return (
+    <span className="badge" style={{ background: colors[status] }}>
+      {status}
+    </span>
+  );
+}
