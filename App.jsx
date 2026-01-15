57567d06cc75c6d0f5e5fdd8085e508668160951
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-15 23:49:34 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Scanner from "./components/Scanner";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>CRT Screener Dashboard</h1>
+      <Scanner />
+    </div>
+  );
+}
