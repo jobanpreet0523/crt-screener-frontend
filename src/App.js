32656c231d944add37a8662c205ab83ce1ba6129
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-01-24 23:32:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 function App() {
   return (
-    <div
-      style={{
-        padding: "20px",
-        color: "white",
-        background: "#0f172a",
-        minHeight: "100vh",
-      }}
-    >
+    <div style={{ padding: "20px" }}>
       <h1>CRT Screener</h1>
       <p>App is working 🎯</p>
     </div>
