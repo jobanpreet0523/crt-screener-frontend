85d578cce68d538079880f01a0e55811f7bd4f81
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-05-31 07:53:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DojiScreener from './crt-dashboard/src/DojiScreener';
-import './crt-dashboard/src/index.css';
+import DojiScreener from './DojiScreener';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
